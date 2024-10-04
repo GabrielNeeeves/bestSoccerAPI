bd7b773185c8154fcef8a5575e9859a8a8d8ce1a
gabrielneeeves <gabrielnevesferreira08@gmail.com>
2024-10-04 09:02:05 -0300

    add scriptDB

---
+++ src/main/resources/db/create.sql
@@ -0,0 +1,55 @@
+-- Criando a tabela Jogador 
+CREATE TABLE JOGADOR (
+	ID SERIAL PRIMARY KEY,
+	NOME VARCHAR(100) NOT NULL,
+	pernaBoa varchar(10) NOT NULL
+	check(pernaBoa IN ('esquerda', 'direita')),
+	posicao VARCHAR(50) NOT NULL,
+	altura DECIMAL(5,2) NOT NULL,
+	peso DECIMAL(5,2) NOT NULL, 
+    contrato DATE NOT NULL, 
+    nacionalidade VARCHAR(50) NOT NULL, 
+    dataNascimento DATE NOT NULL 
+);
+
+-- Criando a tabela Partida 
+CREATE TABLE Partida ( 
+    id serial PRIMARY KEY, 
+    data DATE NOT NULL, 
+    hora TIME NOT NULL, 
+    timeAdversario VARCHAR(100) NOT NULL, 
+    local VARCHAR(100) NOT NULL 
+); 
+
+-- Criando a tabela PartidaJogadores 
+CREATE TABLE PartidaJogadores ( 
+    partidaID INT NOT NULL, 
+    jogadorID INT NOT NULL, 
+    gols INT NOT NULL, 
+    --PRIMARY KEY (partidaID, jogadorID), 
+    FOREIGN KEY (partidaID) REFERENCES Partida(id), 
+    FOREIGN KEY (jogadorID) REFERENCES Jogador(id) 
+); 
+
+--Usuario
+CREATE TABLE usuario (
+	id serial PRIMARY KEY,
+	nome varchar(100) NOT NULL,
+	email VARCHAR(100) NOT NULL UNIQUE, 
+    senha VARCHAR(100) NOT NULL
+);
+
+--UsuarioAdmin
+CREATE TABLE UsuarioAdmin ( 
+    usuarioID serial PRIMARY KEY, 
+    FOREIGN KEY (usuarioID) REFERENCES Usuario(id) 
+);
+
+-- Criando a tabela UsuarioComum 
+CREATE TABLE UsuarioComum ( 
+    usuarioID INT PRIMARY KEY, 
+    FOREIGN KEY (usuarioID) REFERENCES Usuario(id) 
+); 
+
+
+
