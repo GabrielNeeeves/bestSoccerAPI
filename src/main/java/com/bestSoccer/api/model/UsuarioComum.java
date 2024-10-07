6c6077fb1fa62e780c826a41c4531acda78a0aca
gabrielneeeves <gabrielnevesferreira08@gmail.com>
2024-10-07 09:58:21 -0300

    add UsuarioComum

---
+++ src/main/java/com/bestSoccer/api/model/UsuarioComum.java
@@ -0,0 +1,26 @@
+package com.bestSoccer.api.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "UsuarioComum")
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+public class UsuarioComum {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "usuarioID")
+    private Long id;
+
+    @OneToOne
+    @JoinColumn(name = "usuarioID", referencedColumnName = "id", insertable = false, updatable = false)
+    private UsuarioModel usuario;
+
+}
