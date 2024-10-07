30ea19ca31ff48feabe55386a033ba32d4dcab6e
gabrielneeeves <gabrielnevesferreira08@gmail.com>
2024-10-07 08:14:40 -0300

    add jogador model

---
+++ src/main/java/com/bestSoccer/api/model/JogadorModel.java
@@ -0,0 +1,51 @@
+package com.bestSoccer.api.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
+@Entity
+@Table(
+        name = "Jogador",
+        uniqueConstraints = @UniqueConstraint(columnNames = "id")
+)
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class JogadorModel {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, length = 100)
+    private String nome;
+
+    @Column(nullable = false, length = 10)
+    private String pernaBoa;
+
+    @Column(nullable = false, length = 50)
+    private String posicao;
+
+    @Column(nullable = false, precision = 5, scale = 2)
+    private BigDecimal altura;
+
+    @Column(nullable = false, precision = 5, scale = 2)
+    private BigDecimal  peso;
+
+    @Column(nullable = false)
+    private LocalDate contrato;
+
+    @Column(nullable = false, length = 50)
+    private String nacionalidade;
+
+    @Column(nullable = false)
+    private LocalDate dataNascimento;
+
+}
